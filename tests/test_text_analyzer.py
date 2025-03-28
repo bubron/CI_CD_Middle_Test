297952d50b476cc7b0a8110619af95b9111b93a6
bubron <143940924+bubron@users.noreply.github.com>
2025-03-28 10:08:50 +0200

    Added fixture sample_text_with_commas function to test_text_analyzer.py

--- tests/test_text_analyzer.py
+++ tests/test_text_analyzer.py
@@ -4,3 +4,7 @@ from text_analyzer import count_words, count_sentences, analyze_file
 @pytest.fixture
 def sample_text():
     return "Це перше речення. А це друге! А це третє, чи не так? Четверте... Ну і п'яте."
+
+@pytest.fixture
+def sample_text_with_commas():
+    return "Слово, ще слово: третє; і останнє."
