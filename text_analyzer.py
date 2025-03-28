b6be64ba0d87beb956672dd45caa68a5a6a39258
bubron <143940924+bubron@users.noreply.github.com>
2025-03-28 09:28:52 +0200

    Added count_words function to text_analyzer.py

--- text_analyzer.py
+++ text_analyzer.py
@@ -5,3 +5,9 @@ def count_sentences(text: str) -> int:
     sentence_endings = r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!|\.{3})\s'
     sentences = re.split(sentence_endings, text.strip())
     return len([s for s in sentences if s])
+
+def count_words(text: str) -> int:
+    """Підраховує кількість слів у тексті"""
+    word_separators = r'[,\s:;]+'
+    words = re.split(word_separators, text.strip())
+    return len([w for w in words if w])
